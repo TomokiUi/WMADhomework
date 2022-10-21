8f099af27cc185a1559223d682e778d53128e063
Tomki Ui <uiuijr0918@gmail.com>
2022-10-20 20:37:42 -0700

    java

---
+++ java/excercise1.java
@@ -0,0 +1,19 @@
+package Fruits;
+
+import Vehicle.Vehicle;
+
+public class Fruits extends Vehicle {
+    private int Name;
+    private int Color;
+    private int Weight;
+    private boolean isTasty;
+    private int Price;
+    private int Quantity;
+
+    public Car() {
+
+    }
+
+    
+
+
